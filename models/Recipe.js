2231a187066bbb4cefd5af8fb9f4cf891e5639c9
Lilach Ritter <lilach@gmail.com>
2018-01-14 20:26:52 +0200

    removed double files

--- models/Recipe.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-module.exports = new mongoose.Schema({
-    name: String,
-    image: String,
-    recipe_url: String,
-	ingredients: [String],
-    id: mongoose.Schema.ObjectId
-})
